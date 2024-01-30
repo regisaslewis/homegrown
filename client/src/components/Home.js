8a7a642b43ceba71addafbe22ea0c6052e5917b7
RegisLewis <regisaslewis@gmail.com>
2024-01-30 15:12:29 -0800

    add plant button functional

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,6 +12,7 @@ function Home() {
     const userConditional = Object.keys(loggedUser).length > 0 ? loggedUser.name : ""
     const userLikedArticles = loggedUser.liked_articles.map(e => <p key={e.id}>{e.body}</p>);
     const userDislikeArticles = loggedUser.disliked_articles.map(e => <p key={e.id}>{e.body}</p>)
+    const userPlants = loggedUser.plants.map(e => <p key={e.id}>{e.name}</p>)
 
     useEffect(() => {
         dispatch(switchButton(1))
@@ -21,8 +22,12 @@ function Home() {
         <div>
             <h2>Home Page</h2>
             <h4>Current User = {userConditional}</h4>
+            <p>_____________</p>
+            {userConditional}'s Plants: {userPlants}
+            <p>_____________</p>
             Liked Articles:
             <br />{userConditional ?  userLikedArticles : ""}
+            <p>_____________</p>
             Disliked Articles:
             <br />{userConditional ? userDislikeArticles : ""}
         </div>
